2c89f757997fc0ee7e11a46710a95f927e20ebf0
Marcio <marcio.correa@live.com>
2020-05-16 23:11:18 -0400

    Added friends page, friend search, updated model with search method, to find term in first name, last name and email.

--- app/controllers/friendship_controller.rb
+++ app/controllers/friendship_controller.rb
@@ -8,7 +8,8 @@ class FriendshipController < ApplicationController
     friend = User.find(params[:id])
     friendship = Friendship.where(user_id: current_user.id, friend_id: friend.id).first
     friendship.destroy
-    flash[:notice] = "#{friend.first_name} was unfriended 😢."
+    sass = rand(3)
+    flash[:notice] = "#{friend.first_name} unfollowed! #{["💁‍♀️ - Bye Felicia!","💁‍♂️","✂","💅","🧊",].shuffle.first}"
     redirect_to friends_path
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'friends', to: 'users#friends'
+  get 'search_friend', to: 'users#search'
   get 'search_stock', to: 'stocks#search'
 
 end
